dde17bac025ab0546dc60c56c5d1058defc21da1
Robert Kehoe <robert.kehoe@f-secure.com>
2013-08-09 13:00:23 +0800

    Adding basic support for IE 6/7/8

--- js/blink.js
+++ js/blink.js
@@ -1,11 +1,23 @@
-angular.module('components', []).
-  directive('blink', function() {
+angular.module('ng-blink', []).
+  directive('blink', function($timeout) {
     return {
       restrict: 'E',
       transclude: true,
       template:
         '<div ng-transclude class="ng-blink">' +
           '</div>',
-      replace: true
+      replace: true ,
+      controller: function($scope, $element) {
+
+        // IE 6/7/8 don't support CSS animations, so fall back to Javascript
+        if ( navigator.userAgent &&  navigator.userAgent.match(/MSIE [6-8]/i)) {
+          setInterval(function () {
+            $element.addClass("hidden");
+            $timeout(function () {
+              $element.removeClass("hidden");
+            }, 250);
+          }, 750);
+        }
+      }
     };
   });
